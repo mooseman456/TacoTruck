75b6d751b8387d2bb1a3aadb835d4dad2ef7d337
LukeOglesbee <loglesbee@smu.edu>
2014-02-26 16:56:40 -0600

    Map with dummy pins.

--- Web/js/locations.js
+++ Web/js/locations.js
@@ -1,8 +1,22 @@
 window.addEventListener('load', function() {
 	
 	// Initialize Google Map
-	var mapOptions = {center: new google.maps.LatLng(-34.397, 150.644),zoom: 8 };
+	var truck1LatLng = new google.maps.LatLng(32.73, -96.8);
+	var truck2LatLng = new google.maps.LatLng(32.8, -97);
+	var mapOptions = {center: new google.maps.LatLng(32.73, -96.8),zoom: 11 };
 	var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-	google.maps.event.addDomListener(window, 'load', initialize);
+	//google.maps.event.addDomListener(window, 'load', initialize);
+
+	var truck1 = new google.maps.Marker( {
+		position: truck1LatLng,
+		map: map,
+		title: 'One Truck!'
+	});
+
+	var truck2 = new google.maps.Marker( {
+		position: truck2LatLng,
+		map: map,
+		title: 'Two Truck!'
+	});
 
 }, false);
