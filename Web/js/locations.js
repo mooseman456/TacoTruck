7ec7f23852b5d820a8b1d9759541d16419144f2d
LukeOglesbee <loglesbee@smu.edu>
2014-02-27 12:06:16 -0600

    Able to query the JSON file.  Correctly display the location of one trcuk from the JSON file.

--- Web/js/locations.js
+++ Web/js/locations.js
@@ -7,6 +7,34 @@ window.addEventListener('load', function() {
 	var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
 	//google.maps.event.addDomListener(window, 'load', initialize);
 
+	// Trucks from json
+	geocoder = new google.maps.Geocoder();
+	var request = new XMLHttpRequest();
+	request.open("GET", "resources/taco_truck_locations.json", false);
+	request.send();
+
+	if (request.status === 200) {
+		console.log("it worked!");
+		var trucks = JSON.parse(request.responseText);
+		
+		geocoder.geocode( { 'address': trucks[0].address + ", " + trucks[0].city + ", " + trucks[0].state + " " + trucks[0].zipcode}, function(results, status) {
+		    if (status == google.maps.GeocoderStatus.OK) {
+		        // map.setCenter(results[0].geometry.location);
+		        var marker = new google.maps.Marker({
+		            map: map,
+		            position: results[0].geometry.location,
+		            title: trucks[0].name
+		        });
+		    } else {
+		        alert("Geocode was not successful for the following reason: " + status);
+		    }
+		});
+	} else {
+		window.alert("Could not get truck locations.")
+	}
+
+	// Dummy Trucks
+/*
 	var truck1 = new google.maps.Marker( {
 		position: truck1LatLng,
 		map: map,
@@ -18,5 +46,6 @@ window.addEventListener('load', function() {
 		map: map,
 		title: 'Two Truck!'
 	});
+*/
 
 }, false);
