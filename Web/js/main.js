be4980b3d541d53161e2feaf39f8309129d4c125
soxymo <soxymo@aol.com>
2014-03-01 18:42:20 -0600

    Revert "Fixed cancel buttons"
    This reverts commit 509dd022f37fa85b219f785b5445ef77b4fcae13.

--- Web/js/main.js
+++ Web/js/main.js
@@ -2,9 +2,8 @@ document.getElementById("logoImg").onclick = function() {
     window.location = "index.html";
 }
 
-var cancelButton = document.getElementsByClassName('cancelButton');
-for (var i=0; i<cancelButton.length; i++)
-   cancelButton[i].addEventListener('click', clickCancel, false);
+var cancelButton = document.getElementById('cancelButton');
+cancelButton.addEventListener('click', clickCancel, false);
 
 function clickCancel() {
 	alert("You clicked cancel");
