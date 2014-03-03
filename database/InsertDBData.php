a448544c11e97d385521599f686df5b2aa28e8dd
moosemn456 <jgstangelo@gmail.com>
2014-03-03 14:40:12 -0600

    Fixed the insertion of data

--- database/InsertDBData.php
+++ database/InsertDBData.php
@@ -18,7 +18,7 @@ if (($handle = fopen("Users.csv", "r")) !== FALSE) {
         $num = count($data);
         $query = "INSERT INTO Users (user_id, givenName, surname, email, password, phoneNumber, CC_Provider, CC_Number) 
         		  VALUES ('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]','$data[7]')";
-        //$result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
+        $result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
     }
     fclose($handle);
 }
@@ -42,11 +42,11 @@ if (($handle = fopen("taco_truck_menu.json", "r")) !== FALSE) {
 				$heatRating = $item['heatRating'];
 				echo "$name (heat: $heatRating) - $price<br />";
 				$query = "INSERT INTO $table (name, price, heatRating) VALUES ('$name', '$price', '$heatRating')";
-				//$result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
+				$result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
 			} else {
 				echo "$name - $$price<br />";
 				$query = "INSERT INTO $table (name, price) VALUES ('$name', '$price')";
-				//$result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
+				$result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
 			}
 		}
 		echo "<br /><hr /><br />";
@@ -75,7 +75,7 @@ foreach ($jsonIterator as $key => $val) {
     		$price = $val;
     		echo $out;
     		$query = "INSERT INTO TacoFixings (itemType, name, price, heatRating) VALUES ('$itemType', '$name', '$price', '$heatRating')";
-    		//$result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
+    		$result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
     		$name=NULL;$price=NULL;$heatRating=NULL;
     	}
         
@@ -94,7 +94,7 @@ if (($handle = fopen("Orders.csv", "r")) !== FALSE) {
         $query = "INSERT INTO Orders (user_id, price, timePlaced) 
         		  VALUES ('$data[1]','$data[3]','$data[2]')";
         echo "$query";
-        //$result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
+        $result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
     }
     fclose($handle);
 }
@@ -112,7 +112,7 @@ if (($handle = fopen("OrderItem.csv", "r")) !== FALSE) {
         $query = "INSERT INTO TacoOrders (order_id, quantity) 
         		  VALUES ('$data[1]', '$data[2]')";
         echo "$query";
-        //$result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
+        $result = $db->query($query)  or trigger_error($mysqli->error."[$query]"); //This line does the query
     }
     fclose($handle);
 }
