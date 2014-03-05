66af782af01175eb995d5021900212cb8454c5df
Alexander Siems <alexandersiems@usr220-151.dyn4.smu.edu>
2014-03-05 14:44:07 -0600

    Alec

--- Web/account.php
+++ Web/account.php
@@ -25,7 +25,7 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
 		$row = $result->fetch_assoc();
 
 		if(empty($row)) {
-			$loginStatus = "Login Failed";
+			$loginStatus = "<p style=\"text-align:center;\">Login Failed</p>";
 		} else {
 			session_start();
 
