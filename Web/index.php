200424d25ac04af1405226baaf80f51fa9af6088
Alexander Siems <alexandersiems@usr220-151.dyn4.smu.edu>
2014-03-05 16:04:23 -0600

    Yoyoyo

--- Web/index.php
+++ Web/index.php
@@ -6,29 +6,48 @@ if($db->connect_errno > 0){
 	die('Unable to connect to database [' . $db->connect_error . ']');
 }
 
-//Get Premade Tacos
-$query = "SELECT PreMadeTacos.tacoorder_id, PreMadeTacos.name, PreMadeTacos.description FROM PreMadeTacos";
-$result = $db->query($query)  or trigger_error($mysqli->error."[$query]");
-$i = 0;
-while($data = $result->fetch_assoc()) {
-	$PreMadeTacos[$i] = $data;
-	$i += 1;
+//Menu Items JSON
+if (($handle = fopen("../database/premade_tacos.json", "r")) !== FALSE) {
+	
+	$json = file_get_contents('../database/premade_tacos.json');
+	$data = json_decode($json,true);
+	$counter = 0;
+
+	foreach ($data['PreMadeTacos'] as $taco) {
+		$PreMadeTacos[$counter] = $taco;
+		$PreMadeTacos[$counter]['ingredients'] = $PreMadeTacos[$counter]['type'].", ".$PreMadeTacos[$counter]['tortilla'].", ".
+												 $PreMadeTacos[$counter]['rice'].", ".$PreMadeTacos[$counter]['cheese'].", ".
+												 $PreMadeTacos[$counter]['beans'].", ".$PreMadeTacos[$counter]['sauces'].", ".
+												 $PreMadeTacos[$counter]['vegetables'].", ".$PreMadeTacos[$counter]['extras'];
+		//Remove trailing space and comma
+		$PreMadeTacos[$counter]['ingredients'] = rtrim($PreMadeTacos[$counter]['ingredients'],', ');
+		$counter++;
+	}
 }
 
-foreach($PreMadeTacos as $key => $val) {
-	//Get Fillings
-	$query = "SELECT TacoFixings.name, TacoFixings.price FROM TacoDetails JOIN TacoFixings 
-			  ON TacoDetails.tacofixing_id=TacoFixings.tacofixing_id JOIN TacoOrders 
-			  ON TacoDetails.tacoorder_id=TacoOrders.tacoorder_id 
-			  WHERE TacoOrders.tacoorder_id='".$PreMadeTacos[$key]['tacoorder_id']."'";
-	$result = $db->query($query)  or trigger_error($mysqli->error."[$query]");
-	$PreMadeTacos[$key]['ingredients'] = "\nIngredients: ";
-	while($data = $result->fetch_assoc()) {
-		$PreMadeTacos[$key]['ingredients'] = $PreMadeTacos[$key]['ingredients'] .  $data['name'] . ", ";
-	}
-	//Remove trailing space and comma
-	$PreMadeTacos[$key]['ingredients'] = rtrim($PreMadeTacos[$key]['ingredients'],', ');
-}
+// //Get Premade Tacos
+// $query = "SELECT PreMadeTacos.tacoorder_id, PreMadeTacos.name, PreMadeTacos.description FROM PreMadeTacos";
+// $result = $db->query($query)  or trigger_error($mysqli->error."[$query]");
+// $i = 0;
+// while($data = $result->fetch_assoc()) {
+// 	$PreMadeTacos[$i] = $data;
+// 	$i += 1;
+// }
+
+// foreach($PreMadeTacos as $key => $val) {
+// 	//Get Fillings
+// 	$query = "SELECT TacoFixings.name, TacoFixings.price FROM TacoDetails JOIN TacoFixings 
+// 			  ON TacoDetails.tacofixing_id=TacoFixings.tacofixing_id JOIN TacoOrders 
+// 			  ON TacoDetails.tacoorder_id=TacoOrders.tacoorder_id 
+// 			  WHERE TacoOrders.tacoorder_id='".$PreMadeTacos[$key]['tacoorder_id']."'";
+// 	$result = $db->query($query)  or trigger_error($mysqli->error."[$query]");
+// 	$PreMadeTacos[$key]['ingredients'] = "\nIngredients: ";
+// 	while($data = $result->fetch_assoc()) {
+// 		$PreMadeTacos[$key]['ingredients'] = $PreMadeTacos[$key]['ingredients'] .  $data['name'] . ", ";
+// 	}
+// 	//Remove trailing space and comma
+// 	$PreMadeTacos[$key]['ingredients'] = rtrim($PreMadeTacos[$key]['ingredients'],', ');
+// }
 
 ?>
 
@@ -93,8 +112,8 @@ foreach($PreMadeTacos as $key => $val) {
 				<td>Taco 4</td>
 			</tr>
 			<tr class="tacoRow">
-				<td><img src="img/taco_icon.png" alt="Taco 1" title=<?php echo "\"".$PreMadeTacos[0]['description'].$PreMadeTacos[0]['ingredients']."\""; ?>></td>
-				<td><img src="img/taco_icon.png" alt="Taco 2" title=<?php echo "\"".$PreMadeTacos[1]['description'].$PreMadeTacos[1]['ingredients']."\""; ?>></td>
+				<td><img src="img/taco_icon.png" alt="Taco 1" title=<?php echo "\"".$PreMadeTacos[0]['description']." Ingredients: ".$PreMadeTacos[0]['ingredients']."\""; ?>></td>
+				<td><img src="img/taco_icon.png" alt="Taco 2" title=<?php echo "\"".$PreMadeTacos[1]['description']." Ingredients: ".$PreMadeTacos[1]['ingredients']."\""; ?>></td>
 				<td><img src="img/taco_icon.png" alt="Taco 3"></td>
 				<td><img src="img/taco_icon.png" alt="Taco 4"></td>
 			</tr>
